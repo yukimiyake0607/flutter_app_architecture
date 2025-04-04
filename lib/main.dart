e05ce0699d8f9c39192daec0c978e8c3bfb645ff
yukimiyake0607 <yukimiyake38@gmail.com>
2025-04-04 16:39:02 +0900

    add: 環境変数の対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_architecture/firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 環境変数の読み込み
+  await dotenv.load(fileName: ".env");
+  
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
