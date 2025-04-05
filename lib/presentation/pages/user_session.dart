969ba27e1944538c73d14a37ca5338226dbd647b
yukimiyake0607 <yukimiyake38@gmail.com>
2025-04-05 17:08:26 +0900

    add: 最初をAuthPageに設定

--- lib/presentation/pages/user_session.dart
+++ lib/presentation/pages/user_session.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_architecture/presentation/pages/auth_page.dart';
 
 class UserSession extends StatelessWidget {
   const UserSession({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AuthPage();
   }
 }
