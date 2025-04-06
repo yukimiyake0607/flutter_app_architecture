4103e4184299520664ea5ec868023a5e24d9c7f6
yukimiyake0607 <yukimiyake38@gmail.com>
2025-04-06 22:01:49 +0900

    add: auth_serviceを追加
    - Resultクラスに変換する責務を担当

---
+++ lib/application/services/auth_service.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_app_architecture/data/repositories/auth_repository_impl.dart';
+import 'package:flutter_app_architecture/domain/models/result/result.dart';
+import 'package:flutter_app_architecture/domain/models/user_model/user_model.dart';
+import 'package:flutter_app_architecture/domain/repositories/i_auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authServiceProvider = Provider<AuthService>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AuthService(authRepository);
+});
+
+class AuthService {
+  final IAuthRepository _authRepository;
+
+  const AuthService(this._authRepository);
+
+  Future<Result<UserModel, Exception>> register(
+    String email,
+    String password,
+  ) async {
+    try {
+      final user = await _authRepository.register(email, password);
+      return Result.success(user!);
+    } on Exception catch (e) {
+      return Result.failure(e);
+    }
+  }
+
+  Future<Result<UserModel, Exception>> login(
+    String email,
+    String password,
+  ) async {
+    try {
+      final user = await _authRepository.login(email, password);
+      return Result.success(user!);
+    } on Exception catch (e) {
+      return Result.failure(e);
+    }
+  }
+
+  Future<Result<void, Exception>> logout() async {
+    try {
+      await _authRepository.logout();
+      return Result.success(null);
+    } on Exception catch (e) {
+      return Result.failure(e);
+    }
+  }
+
+  Stream<Result<UserModel?, Exception>> get authStateChanges {
+    return _authRepository.authStateChanges.map((user) {
+      try {
+        return Result.success(user);
+      } on Exception catch (e) {
+        return Result.failure(e);
+      }
+    });
+  }
+}
