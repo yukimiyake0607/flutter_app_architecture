a57c667b0c14c0565d394cab9fbfbd28fd7523ef
yukimiyake0607 <yukimiyake38@gmail.com>
2025-04-08 10:41:12 +0900

    add: home_pageにログアウトボタンを追加

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_architecture/presentation/controllers/auth_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await ref.read(authControllerProvider.notifier).logout();
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: const Center(
         child: Text('HomePage'),
       ),
     );
